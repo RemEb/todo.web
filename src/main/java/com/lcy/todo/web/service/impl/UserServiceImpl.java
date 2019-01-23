cd77803919a10cf5caf8d4c1274b1a8bbeddd181
lcy <970557317@qq.com>
2019-01-23 22:56:38 +0800

    2019年1月23日 22:56:33

--- src/main/java/com/lcy/todo/web/service/impl/UserServiceImpl.java
+++ src/main/java/com/lcy/todo/web/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.lcy.todo.web.service.impl;
 import com.lcy.todo.web.model.Result;
 import com.lcy.todo.web.model.User;
 import com.lcy.todo.web.service.UserService;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,5 +20,17 @@ public class UserServiceImpl implements UserService {
     @Override
     public boolean activate(String userId) {
         return false;
+    }
+
+    /**
+     * 给密码加密
+     *
+     * @param pwd
+     * @param slat
+     * @return
+     */
+    public static String m5Password(String pwd,String slat) {
+        String md5Hex = DigestUtils.md5Hex(pwd+slat).toUpperCase();
+        return md5Hex;
     }
 }
